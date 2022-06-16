7a7bb996362d85b9a4ca4852e9ef7790f784bc5b
AlxndrThOk <alex.tran24@ihsd.us>
2022-06-15 21:32:29 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        double age = 0;
+        double AGE_CUTOFF = 21;
+        System.out.println("What is your age?");
+        age = 2;
+        System.out.println("Your age is " + age);
+        if (age >= AGE_CUTOFF)
+        {
+            System.out.println("You get a wrist band");
+        }
+
+    }
+}
